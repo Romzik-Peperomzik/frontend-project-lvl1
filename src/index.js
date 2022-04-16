e2985b598099e6eff1586339f0328e46787cf929
Romzik-Peperomzik <warriorway5657@gmail.com>
2022-04-16 17:40:08 +0300

    moved all game-event.js logic to index.js.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,54 @@
 import readlineSync from 'readline-sync';
 
-export function gameGreeting(greetingForSpecificGame) {
+function gameGreeting(greetingForSpecificGame) {
   const playerName = readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ');
   console.log(`Hello, ${playerName}!\n${greetingForSpecificGame}`);
   return playerName;
 }
 
-export function playerInput(question) {
+function playerInput(question) {
   return readlineSync.question(`Question: ${question}\nYour answer: `);
 }
 
-export function resultAnnouncement(player, playerAnswer, answerAreCorrectFlag, correctAnswer) {
+function resultAnnouncement(player, playerAnswer, answerAreCorrectFlag, correctAnswer) {
   if (answerAreCorrectFlag) {
     console.log(`Congratulations, ${player}!`);
   } else {
     console.log(`'${playerAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
     console.log(`Let's try again, ${player}!`);
   }
+}
+
+export default function gameEventLoop(answerType, gameFunction, greetingText) {
+  const player = gameGreeting(greetingText);
+  let answerAreCorrectFlag = true;
+  let roundCount = 0;
+  let playerAnswer;
+  let dataToDisplay;
+  let expectedAnswer;
+
+  while (roundCount < 3 && answerAreCorrectFlag) {
+    roundCount += 1;
+    [dataToDisplay, expectedAnswer] = gameFunction();
+    playerAnswer = playerInput(dataToDisplay);
+
+    if (answerType === 'num') {
+      if (Number(playerAnswer) === expectedAnswer) {
+        console.log('Correct!');
+      } else {
+        answerAreCorrectFlag = false;
+      }
+    } else {
+      if ((expectedAnswer && playerAnswer !== 'yes') || (!expectedAnswer && playerAnswer !== 'no')) {
+        answerAreCorrectFlag = false;
+      }
+      if (answerAreCorrectFlag) {
+        console.log('Correct!');
+      }
+    }
+  }
+  if (answerType === 'bool') {
+    expectedAnswer = expectedAnswer ? 'yes' : 'no';
+  }
+  resultAnnouncement(player, playerAnswer, answerAreCorrectFlag, expectedAnswer);
 }
