e01aa456aa85b54ddfcc8cc04a194c00e2c16eb1
Romzik-Peperomzik <warriorway5657@gmail.com>
2022-04-19 00:28:08 +0300

    refactored index.js, removed unnecessary abstraction code.

--- src/index.js
+++ src/index.js
@@ -1,33 +1,20 @@
 import readlineSync from 'readline-sync';
 
-function greetPlayer(greetingForSpecificGame) {
-  const playerName = readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ');
-  console.log(`Hello, ${playerName}!\n${greetingForSpecificGame}`);
-  return playerName;
-}
-
-function getPlayerInput(question) {
-  return readlineSync.question(`Question: ${question}\nYour answer: `);
-}
-
-function displayWrongResult(player, playerAnswer, correctAnswer) {
-  console.log(`'${playerAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-  console.log(`Let's try again, ${player}!`);
-}
-
 export default function startGameLoop(runGameFunction, greetingText) {
-  const player = greetPlayer(greetingText);
   const maxRound = 3;
+  const playerName = readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ');
+  console.log(`Hello, ${playerName}!\n${greetingText}`);
 
   for (let round = 0; round < maxRound; round += 1) {
-    const [dataToDisplay, expectedAnswer] = runGameFunction();
-    const playerAnswer = getPlayerInput(dataToDisplay);
+    const [question, expectedAnswer] = runGameFunction();
+    const playerAnswer = readlineSync.question(`Question: ${question}\nYour answer: `);
 
     if (playerAnswer !== expectedAnswer) {
-      displayWrongResult(player, playerAnswer, expectedAnswer);
+      console.log(`'${playerAnswer}' is wrong answer ;(. Correct answer was '${expectedAnswer}'.`);
+      console.log(`Let's try again, ${playerName}!`);
       return;
     }
     console.log('Correct!');
   }
-  console.log(`Congratulations, ${player}!`);
+  console.log(`Congratulations, ${playerName}!`);
 }
